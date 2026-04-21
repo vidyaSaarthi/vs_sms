4fbaef8cb287d634abd2e5cf59eb26df07e7fbd4
shubh05 <aggarwal.shubham@gmail.com>
2026-04-21 15:56:19 +0530

    1.0

---
+++ launch_sms.bat
@@ -0,0 +1,2 @@
+"C:\Users\vidya\AppData\Local\Microsoft\WindowsApps\python.exe" "%USERPROFILE%\PycharmProjects\vs_sms\app.py"
+pause
---
+++ launch_sms_shubham.bat
@@ -0,0 +1,2 @@
+"%USERPROFILE%\PycharmProjects\student_management_system\venv\Scripts\python.exe" "%USERPROFILE%\PycharmProjects\student_management_system\app.py"
+pause
