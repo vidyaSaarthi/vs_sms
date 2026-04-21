94a8761988b69425a0b94832a77308ef3bf8d285
shubh05 <aggarwal.shubham@gmail.com>
2026-04-21 16:13:05 +0530

    Removed additional files from tracking

--- launch_sms_shubham.bat
+++
@@ -1,2 +0,0 @@
-"%USERPROFILE%\PycharmProjects\student_management_system\venv\Scripts\python.exe" "%USERPROFILE%\PycharmProjects\student_management_system\app.py"
-pause
--- merge_dbs_shubham.bat
+++
@@ -1,2 +0,0 @@
-"C:\Users\Shubham Aggarwal\PycharmProjects\student_management_system\venv\Scripts\python.exe" "C:\Users\Shubham Aggarwal\PycharmProjects\student_management_system\migrate_db.py"
-pause
