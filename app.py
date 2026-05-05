8b40f01171f627d9f852142cde280a07dc9678ac
shubh05 <aggarwal.shubham@gmail.com>
2026-05-05 17:25:25 +0530

    9.1 - "Changes for student admisison linked to real forms"

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from sqlalchemy import or_
 # Consolidated Imports (🚨 FinanceRecord added here)
 from models import db, Staff, Student, Document, State, StateCategory, University, UniversityCategory, Exam, \
     Counselling, Form, CounsellingRound, RoundSchedule, College, StudentCounsellingRegistration, StudentRoundResult, \
-    Course, StudentExamResult, Task, FormEvent, FinanceRecord
+    Course, StudentExamResult, Task, FormEvent, FinanceRecord, StudentFormSubmission
 
 app = Flask(__name__)
 
